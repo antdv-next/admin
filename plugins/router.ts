71d6cd6e49cf5b00d810612fa5c35cfee6789ad3
aibayanyu20 <aibayanyu@qq.com>
2026-03-02 20:38:17 +0800

    chore: change

--- plugins/router.ts
+++ plugins/router.ts
@@ -1,14 +1,33 @@
 import type { RoutesFolder } from 'vue-router/dist/unplugin/index.mjs'
 
 export function loadRouter() {
+  const commonExclude = [
+    '**/components',
+    '**/hooks',
+    '**/composables',
+  ]
   return [
     {
       src: 'src/pages',
-      path: file => file.slice(file.lastIndexOf('src/pages') + 'src/pages'.length),
+      path: (file) => {
+        return file.slice(file.lastIndexOf('src/pages') + 'src/pages'.length).slice(1)
+      },
       exclude: [
-        '**/components',
-        '**/hooks',
-        '**/composables',
+        ...commonExclude,
+      ],
+    },
+    {
+      src: 'apps/admin',
+      filePatterns: [
+        'pages/**',
+      ],
+      path: (file) => {
+        const prefix = 'apps/admin'
+        const path = file.slice(file.lastIndexOf(prefix) + prefix.length + 1).replace('pages/', '')
+        return `admin/${path}`
+      },
+      exclude: [
+        ...commonExclude,
       ],
     },
   ] as RoutesFolder
