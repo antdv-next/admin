a8add9d4e53c7bab76f9581198bb79053345d0e6
aibayanyu20 <aibayanyu@qq.com>
2026-03-17 07:25:28 +0800

    feat: add info

--- apps/admin/layouts/default/index.vue
+++ apps/admin/layouts/default/index.vue
@@ -1,24 +1,36 @@
 <script setup lang="ts">
+import type { ConfigProviderProps } from 'antdv-next'
+import { useGlobalToken } from '@/composables/token'
+
+type ThemeType = NonNullable<ConfigProviderProps['theme']>
+type ComponentsType = NonNullable<ThemeType['components']>
+type LayoutType = NonNullable<ComponentsType['Layout']>
+const { token } = useGlobalToken()
+const themeConfig = computed(() => {
+  const layoutConfig: LayoutType = {
+    colorBgLayout: 'transparent',
+    headerHeight: 56,
+    siderBg: token.value?.colorBgContainer,
+  }
+
+  return {
+    components: {
+      Layout: layoutConfig,
+    },
+  } as ThemeType
+})
 </script>
 
 <template>
   <a-config-provider
-    :theme="{
-      components: {
-        Layout: {
-          colorBgLayout: 'transparent',
-          headerHeight: 56,
-          headerBg: '#001529',
-        },
-      },
-    }"
+    :theme="themeConfig"
   >
     <a-layout class="min-h-screen">
       <a-layout-header>
         header
       </a-layout-header>
       <a-layout>
-        <a-layout-sider>
+        <a-layout-sider :width="245" :collapsed-width="56">
           sider
         </a-layout-sider>
         <a-layout>
