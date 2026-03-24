ecce8123631c56a36f790b80fbfdf8a6104b1db3
aibayanyu20 <aibayanyu@qq.com>
2026-03-25 06:55:40 +0800

    refactor: use viteplus replace vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import type { UserConfig } from 'vite-plus'
-import { fileURLToPath } from 'node:url'
-import { defineConfig, loadEnv } from 'vite-plus'
-import { loadPlugins } from './plugins'
-import { loadAlias } from './plugins/alias'
-import { loadSever } from './plugins/server'
+import type { UserConfig } from "vite-plus";
+import { fileURLToPath } from "node:url";
+import { defineConfig, loadEnv } from "vite-plus";
+import { loadPlugins } from "./plugins";
+import { loadAlias } from "./plugins/alias";
+import { loadSever } from "./plugins/server";
 
-const baseUrl = fileURLToPath(new URL('./', import.meta.url))
+const baseUrl = fileURLToPath(new URL("./", import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, baseUrl)
+  const env = loadEnv(mode, baseUrl);
   return {
-    base: env.VITE_BASE_PATH ?? '/',
+    base: env.VITE_BASE_PATH ?? "/",
     plugins: loadPlugins(mode, baseUrl),
     resolve: {
       alias: loadAlias(baseUrl),
@@ -25,8 +25,33 @@ export default defineConfig(({ mode }) => {
         // devtools: {},
       },
     },
-    staged: {
-      '*.{js,ts,tsx,vue,json,css,less,sass,scss,md}': 'vp check --fix',
+    lint: {
+      ignorePatterns: [
+        "dist/**",
+        "node_modules/**",
+        "idea",
+        ".output/**",
+        ".agents",
+        ".claude",
+        "types/auto-imports.d.ts",
+        "types/components.d.ts",
+        "types/vue-router.d.ts",
+        "types/layout-generated.d.ts",
+      ],
+      plugins: ["vue", "jsdoc", "import", "vitest", "typescript"],
+      rules: {},
+      options: {
+        typeAware: true,
+        typeCheck: true,
+      },
     },
-  } as UserConfig
-})
+    fmt: {
+      semi: true,
+      singleQuote: true,
+      jsxSingleQuote: true,
+    },
+    staged: {
+      "*.{js,ts,tsx,vue,json,css,less,sass,scss,md}": "vp check --fix",
+    },
+  } as UserConfig;
+});
