84018d04b9f6a1123aca84928db276a85a39a2c1
aibayanyu20 <aibayanyu@qq.com>
2026-04-11 10:55:53 +0800

    chore: update antd

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import antdPlugins from '@enum-plus/plugin-antd'
+import { Enum } from 'enum-plus'
 import { setupComponent } from '@/components/setup.ts'
 import { setupI18n } from '@/locales'
 import App from './App.vue'
 import { router } from './router'
 import './assets/global.css'
+
+// use antd plugin for enum
+Enum.install(antdPlugins)
 
 const main = async () => {
   const app = createApp(App)
